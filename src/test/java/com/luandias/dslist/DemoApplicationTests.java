c34a4da37a13887c66f280f34f8acd1d2cde5b68
Luan <luandsvt@gmail.com>
2024-11-12 19:53:17 -0300

    Project started

---
+++ src/test/java/com/luandias/dslist/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandias.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
